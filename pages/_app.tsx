37ccf797ba165876b8d5a1bedf2c2e77914231ec
Kevin Berridge <kevin.w.berridge@gmail.com>
2025-01-29 21:15:24 -0500

    new font: Petrona

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,22 @@ import 'purecss/build/grids-responsive-min.css'
 import '../styles/global.css'
 import { AppProps } from 'next/app';
 import { Source_Sans_3 } from 'next/font/google'
+import { Petrona } from 'next/font/google'
 
-const sourceSans = Source_Sans_3({ weight: '400', subsets: ['latin'] })
+const sourceSans = Source_Sans_3({ weight: '400', subsets: ['latin'], variable: "--sourcesans" })
+const petrona = Petrona({ weight: '400', subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps } : AppProps) {
   return (
-    <>
+    <div className={sourceSans.variable}>
       <style jsx global>{`
         html, button, input, select, textarea, .pure-g [class *= "pure-u"] {
-          font-family: ${sourceSans.style.fontFamily}, sans-serif;
+          font-family: ${petrona.style.fontFamily};
           font-weight: 400;
           font-size: 17px;
         }
       `}</style>
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
